d6e43acfb53283374eceb279dfd1ee6fa1d3c5aa
Adenrifalgi <115140015+Adenrifalgi@users.noreply.github.com>
2023-10-19 10:28:16 +0700

    Add files via upload

---
+++ Tugas3 No2.js
@@ -0,0 +1,16 @@
+//Node.js 10.14.0
+const restaurant = [
+    {
+      name: "Shikamaru",
+      city: "Ciemas",
+      favoriteDrink: "Teh macha Milk",
+      favoriteFood: "Telor gulung",
+      isVegan: true,
+    },
+  ];
+  
+  console.log("Nama : " + restaurant[0].name);
+  console.log("Kota : " + restaurant[0].city);
+  console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+  console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+  console.log("Vegan : " + restaurant[0].isVegan);
